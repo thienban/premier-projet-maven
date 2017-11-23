ea204ad9a20796f3020ca52b28c572357483faf0
thien ban BUI <thienban91@hotmail.fr>
2017-11-23 15:38:00 +0100

    final2

--- src/main/java/dev/App.java
+++ src/main/java/dev/App.java
@@ -1,5 +1,6 @@
 package dev;
 
+import java.util.ResourceBundle;
 import com.github.lalyos.jfiglet.FigletFont;
 
 /**
@@ -10,7 +11,12 @@ public class App
 {
     public static void main( String[] args )
     {
-    	String asciiArt = FigletFont.convertOneLine("hello");
+    	String titre = ResourceBundle.getBundle("application").getString("titre");
+    	String asciiArt = FigletFont.convertOneLine(titre);
         System.out.println( asciiArt );
+        
+        String environnement = ResourceBundle.getBundle("application").getString(
+        "environnement");
+        System.out.println("Environnement : " + environnement);
     }
 }
